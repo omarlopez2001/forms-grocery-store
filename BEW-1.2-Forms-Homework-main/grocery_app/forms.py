6d2ab27aac463c1bc054afa025677ec7f28ce93c
omarlopez2001 <omarlopez9445@gmail.com>
2021-02-19 22:21:10 -0500

    added to forms.py

--- BEW-1.2-Forms-Homework-main/grocery_app/forms.py
+++ BEW-1.2-Forms-Homework-main/grocery_app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
+from grocery_app.models import GroceryItem, GroceryStore, ItemCategory
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
